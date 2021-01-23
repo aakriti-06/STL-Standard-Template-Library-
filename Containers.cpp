b2e1bcf6b0e4cfefb2d3c3aece48b5e70e264c20
aakriti-06 <74606667+aakriti-06@users.noreply.github.com>
2021-01-23 22:43:03 +0530

    Create Containers.cpp

---
+++ Containers.cpp
@@ -0,0 +1,13 @@
+*COMMON CONTAINERS:
+1.vectors:replicates vectors(arrays)
+2.queue:replicates queue
+3.list:replicates linked list
+4.set:replicates tree
+5.priority_queue:replicates heap
+6.stack:replicates stack
+7.map:associative arrays
+
+1. Sequence containers or linear: arrays,linked list,etc.
+2. Associative containers: sorted data structure,map,set,etc.
+3. Unordered containers: unordered data structures
+4. Associative adapters: interface to sequence containers
